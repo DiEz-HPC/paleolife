0b8d9ca75e84a3118df1d38b71371f5987ab4b27
RedPandore <houry9@gmail.com>
2022-09-07 17:13:07 +0200

    deploy

--- docker-entry.sh
+++ docker-entry.sh
@@ -3,6 +3,8 @@ set -e
 
 php composer.phar update
 
+npm run build
+
 ## Symfony configuration
 if [ ${APP_ENV} != "prod" ]; then
   php bin/console doctrine:database:drop --force --quiet --if-exists --no-interaction
